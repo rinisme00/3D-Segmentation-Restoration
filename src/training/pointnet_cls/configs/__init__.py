52859e7c4a32878955307b9e05661047cb433373
rinisme00 <ringamer2308@gmail.com>
2026-07-01 16:00:45 +0700

    remove inactive classification and PointNet assets

--- src/training/pointnet_cls/configs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .default import BASE_CONFIG, LABEL_NAMES, SMOKE_CONFIG, build_config, print_config
-
-__all__ = [
-    "BASE_CONFIG",
-    "LABEL_NAMES",
-    "SMOKE_CONFIG",
-    "build_config",
-    "print_config",
-]
